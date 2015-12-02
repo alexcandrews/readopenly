2584e1fabfcb7aa81f595b13fc88558c987c37bb
Alex Christian Andrews <andrew53@maine.cs.byu.edu>
2015-12-02 12:52:01 -0700

    styled the menu; in progress

--- js/app.js
+++ js/app.js
@@ -6,33 +6,32 @@ var App = React.createClass({
   render: function() {
     return (
 
-<div>
+      <div>
         <nav className="navbar navbar-default" role="navigation">
-          <div className="container">
-              <div className="navbar-header">
-                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                   <span className="sr-only">Toggle navigation</span>
-                   <span className="icon-bar"></span>
-                   <span className="icon-bar"></span>
-                   <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="/">Beginning</a>
-              </div>
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav">
-                  <li><Link to="login">Login</Link></li>
-                  <li><Link to="explore">Explore</Link></li>
-                </ul>
-              </div>
+          <div className="container">  
+	    <a className="navbar-brand" href="/~andrew53/index.html">Home</a>
+            <div className="navbar-header">
+               <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                 <span className="sr-only">Toggle navigation</span>
+                 <span className="icon-bar"></span>
+                 <span className="icon-bar"></span>
+                 <span className="icon-bar"></span>
+              </button>
             </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Link to="login">Login</Link></li>
+                <li><Link to="explore">Explore</Link></li>
+              </ul>
+            </div>
+          </div>
         </nav>
 
         <div className="container">
           {this.props.children}
         </div>
       </div> 
-
-   );
+    );
   }
 });
 
