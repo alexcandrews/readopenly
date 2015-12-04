475daa4bec3e9b1e9edb97c383bb141d7bedc608
Alex C Andrews <alexcandrews91@gmail.com>
2015-12-03 19:18:13 -0700

    Update 2015-12-04T02:18:09.563Z

--- js/app.js
+++ js/app.js
@@ -890,65 +890,16 @@ webpackJsonp([1],{
 	var ReactRouter = __webpack_require__(159);
 	
 	var LibraryHeader = __webpack_require__(216);
-	var ListEntry = __webpack_require__(218);
-	var ListItems = __webpack_require__(219);
+	var LibraryItemList = __webpack_require__(221);
 	
-	var api = __webpack_require__(217);
-	var auth = __webpack_require__(211);
-	
-	// List page, shows the todo list of items
 	var LibraryPage = React.createClass({
 	    displayName: "LibraryPage",
 	
-	    // context so the component can access the router
-	    contextTypes: {
-	        location: React.PropTypes.object
-	    },
-	
-	    // initial state
-	    getInitialState: function () {
-	        return {
-	            // list of items in the todo list
-	            items: []
-	        };
-	    },
-	
-	    // when the component loads, get the list items
-	    componentDidMount: function () {
-	        api.getItems(this.listSet);
-	    },
-	
-	    // reload the list of items
-	    reload: function () {
-	        api.getItems(this.listSet);
-	    },
-	
-	    // callback for getting the list of items, sets the list state
-	    listSet: function (status, data) {
-	        if (status) {
-	            // set the state for the list of items
-	            this.setState({
-	                items: data.items
-	            });
-	        } else {
-	            // if the API call fails, redirect to the login page
-	            this.context.router.transitionTo('/login');
-	        }
-	    },
-	
-	    // Show the list of items. This component has the following children: ListHeader, ListEntry and ListItems
 	    render: function () {
-	        var name = auth.getName();
 	        return React.createElement(
-	            "section",
-	            { id: "todoapp" },
-	            React.createElement(LibraryHeader, { name: name, items: this.state.items, reload: this.reload }),
-	            React.createElement(
-	                "section",
-	                { id: "main" },
-	                React.createElement(ListEntry, { reload: this.reload }),
-	                React.createElement(ListItems, { items: this.state.items, reload: this.reload })
-	            )
+	            "div",
+	            null,
+	            React.createElement(LibraryItemList, null)
 	        );
 	    }
 	});
