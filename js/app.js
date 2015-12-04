4518a6c33fd2a0e0437199cfecba734021def91b
Alex C Andrews <alexcandrews91@gmail.com>
2015-12-04 11:42:59 -0700

    Update 2015-12-04T18:42:57.211Z

--- js/app.js
+++ js/app.js
@@ -345,10 +345,10 @@ webpackJsonp([1],{
 	        return React.createElement(
 	            "div",
 	            null,
-	            React.createElement("input", { type: "text", className: "form-control", placeholder: "what do you want to learn?", value: value, onChange: this.handleChange }),
 	            React.createElement(
 	                "center",
 	                null,
+	                React.createElement("input", { type: "text", className: "form-control col-xs-2", placeholder: "what do you want to learn?", value: value, onChange: this.handleChange }),
 	                React.createElement("input", { className: "btn btn-primary", type: "submit", value: "?" })
 	            )
 	        );
