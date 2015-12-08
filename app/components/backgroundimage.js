18e00901a1d4eea2bdc85552bc9d40565e484ead
mkwmms <mkwmms@users.noreply.github.com>
2015-12-07 21:51:09 -0700

    Code cleanup

--- app/components/backgroundimage.js
+++ app/components/backgroundimage.js
@@ -2,7 +2,7 @@ var React = require("react");
 var ReactRouter = require("react-router");
 
 var BackgroundImage = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="backgroung-image">
                 <img src="galaxy.jpg" alt="readonly"/>
