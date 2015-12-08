4c0009e847766e2ab6f6740df4e96462118faeb0
mkwmms <mkwmms@users.noreply.github.com>
2015-12-08 11:56:34 -0700

    Fix libraryItem db creation & add creation date to schema

--- models/libraryitem.js
+++ models/libraryitem.js
@@ -3,12 +3,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var findOrCreate = require('mongoose-findorcreate');
 
-var libraryItemSchema = new Schema(
-    {
-        url: {type: String, index: true, unique: true},
-        itemtype: String,
-    }
-);
+var libraryItemSchema = new Schema({
+    url: {type: String, index: true, unique: true},
+    itemtype: String,
+    created: {type: Date, default: Date.now}
+});
+
+// ensure schemas use virtual IDs
+libraryItemSchema.set('toJSON', {
+    virtuals: true
+});
+
+// add findorCreate
+libraryItemSchema.plugin(findOrCreate);
 
 // create library item
 var LibraryItem = mongoose.model('libraryitem', libraryItemSchema);
