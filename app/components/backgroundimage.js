641b5f0f6d423938212aec98aec085213291dc42
Alex C Andrews <alexcandrews91@gmail.com>
2015-12-07 21:41:30 -0700

    working on background image

---
+++ app/components/backgroundimage.js
@@ -0,0 +1,14 @@
+var React = require("react");
+var ReactRouter = require("react-router");
+
+var BackgroundImage = React.createClass({
+    render: function() {
+        return (
+            <div className="backgroung-image">
+                <img src="galaxy.jpg" alt="readonly"/>
+            </div>
+        );
+    }
+});
+
+module.exports = BackgroundImage;
