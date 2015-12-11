ca3ac45f1623cab8fbb31ed68a648f7bddab0ad0
mkwmms <mkwmms@users.noreply.github.com>
2015-12-11 01:48:50 -0700

    Add AWS deploy gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
 var webpack = require('webpack-stream');
+var shell = require('gulp-shell')
 
 // Build website
 gulp.task('webpack', function () {
@@ -9,28 +9,8 @@ gulp.task('webpack', function () {
         .pipe(gulp.dest('./public/'));
 });
 
-// Deploy built website to gh-pages
-gulp.task('deploy', function () {
-    return gulp.src('./public/**/*')
-        .pipe(ghPages({
-            message: "Update gh-pages website"
-        }));
-});
-
-// git commit task with gulp prompt
-//gulp.task('commit', function(){
-//    // just source anything here - we just wan't to call the prompt for now
-//    gulp.src('package.json')
-//        .pipe(prompt.prompt({
-//            type: 'input',
-//            name: 'commit',
-//            message: 'Please enter commit message...'
-//        },  function(res){
-//            // now add all files that should be committed
-//            // but make sure to exclude the .gitignored ones, since gulp-git tries to commit them, too
-//            return gulp.src([ '!node_modules/', './*' ], {buffer:false})
-//                .pipe(git.commit(res.commit));
-//        }));
-//});
+gulp.task('deploy', shell.task([
+    'rsync -ahuvPEL . aws:~/readopenly --exclude-from=.gitignore'
+]));
 
 gulp.task('default', ['webpack']);
