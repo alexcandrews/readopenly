821b5c6a695a1e22bf4ecaecb8ccf50dbdee97ab
Alex C Andrews <alexcandrews91@gmail.com>
2015-12-12 15:28:06 -0700

    remove the "learning is free" text

--- app/components/backgroundimage.js
+++ app/components/backgroundimage.js
@@ -6,12 +6,15 @@ var BackgroundImage = React.createClass({
         return (
             <div className="background-image">
                 <img className="img-responsive background-image" src="images/background.jpg" alt="readonly"/>
-                <div className="carousel-caption">
-                    <h1>learning is free.</h1>
-                </div>
             </div>
         );
     }
 });
 
 module.exports = BackgroundImage;
+
+/*
+ <div className="carousel-caption">
+ <h1>learning is free.</h1>
+ </div>
+ */
